0406de7cd55484342f5e0d4e07ae00b7744bf243
Denis Perez <denis.perez@symfintek.com>
2026-01-15 20:05:39 -0400

    Add input and solution files for Day 11 problem implementation

---
+++ 11/solution.py
@@ -0,0 +1,60 @@
+import os
+from typing import List, Tuple, Dict
+from collections import deque
+
+def writeToFile(outputFileName: str, lines: list, h: int, w: int):
+    with open(outputFileName, 'w') as f:
+        for row in range(h):
+            for col in range(w):
+                f.write(lines[row][col])
+            f.write('\n')
+
+def solve_from_file(filename: str) -> int:
+    result = 0
+    graph = {}
+    start = 'you'
+    end = 'out'
+    in_degree = {}
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+                
+            node, adj = line.split(':')
+            adj = list(adj.split())
+
+            if node not in in_degree:
+                in_degree[node] = 0
+
+            for neighbor in adj:
+                if neighbor not in in_degree:
+                    in_degree[neighbor] = 0
+                in_degree[neighbor] += 1
+                if(neighbor not in graph):
+                    graph[neighbor] = []
+
+            graph[node] = adj
+    
+    ways = {node: 0 for node in in_degree.keys()}
+    ways[start] = 1
+
+    queue = deque(d for d in graph if in_degree[d] == 0)
+
+    while queue:
+
+        node = queue.popleft()
+
+        for neighbor in graph[node]:
+            ways[neighbor] += ways[node]
+            in_degree[neighbor] -= 1
+            if in_degree[neighbor] == 0:
+                queue.append(neighbor)
+
+    return ways[end]
+                
+if __name__ == "__main__":
+
+    INPUT_FILENAME = "input.txt"
+    ans = solve_from_file(INPUT_FILENAME)
+    print(f"Solution is: {ans}")
